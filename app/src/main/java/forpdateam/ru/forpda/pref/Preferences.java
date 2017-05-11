cd2a1f91323429554f512334d65bc1e6b4dcb30b
Aleksandr <neizvestniytip@gmail.com>
2017-05-11 19:45:46 +0400

    add preferences

---
+++ app/src/main/java/forpdateam/ru/forpda/pref/Preferences.java
@@ -0,0 +1,20 @@
+package forpdateam.ru.forpda.pref;
+
+import forpdateam.ru.forpda.App;
+
+/**
+ * Created by isanechek on 5/11/17.
+ */
+
+public class Preferences {
+
+    public static class News {
+        public static void enableCompatItem(boolean enable) {
+            App.getInstance().getPreferences().edit().putBoolean("news.list.compat", enable).apply();
+        }
+
+        public static boolean getCompatItem() {
+            return App.getInstance().getPreferences().getBoolean("news.list.compat", false);
+        }
+    }
+}
